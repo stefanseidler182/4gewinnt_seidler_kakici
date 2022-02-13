008770bfb7b25602852ef2176e6c69043dd5a140
stefanseidler182 <s50862@edu.campus02.at>
2022-02-13 11:54:58 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	viergewinnt/main.py

--- viergewinnt/main.py
+++ viergewinnt/main.py
@@ -1,67 +1,53 @@
 # Key = (spalte, zeile), Value = 'O' oder 'X'
-spielfeld = {}
+from typing import Any
 
-# Unser Spielfeld hat 7 Spalten und 6 Zeilen, daraus ergibt sich die Anzahl der Zellen
-SPALTEN = 7
-ZEILEN = 6
-ZELLEN = SPALTEN * ZEILEN
 # Die Richtungen dienen dazu, zu überprüfen, ob ein Spieler (in eine bestimmte Richtung) gewonnen hat.
 RICHTUNGEN = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
-'''
-def quadPositionen(position, richtung):
-    positionen = set()
-    spalte, zeile = position
-    richtung_spalte, richtung_zeile = richtung
-    neue_spalte, neue_zeile = spalte + richtung_spalte * 3, zeile + richtung_zeile * 3
-    if neue_spalte < 0 or neue_spalte >= SPALTEN or neue_zeile < 0 or neue_zeile >= ZEILEN:
-        return False
-    for i in range(4):
-        positionen.add((spalte+richtung_spalte*i, zeile+richtung_zeile*i))
-        return positionen
+
+class Spielfeld:
+    """
+    In der Spielfeld-Klasse befinden sich alle Funktionen, die das Spielfeld betreffen.
+
+    Das Spielfeld selbst, ist ein 2-dimensionales Array mit 6 Zeilen und 7 Spalten.
+    """
+
+    def __init__(self):
+        self._spielfeld = [[".", ".", ".", ".", ".", ".", "."],
+                         [".", ".", ".", ".", ".", ".", "."],
+                         [".", ".", ".", ".", ".", ".", "."],
+                         [".", ".", ".", ".", ".", ".", "."],
+                         [".", ".", ".", ".", ".", ".", "."],
+                         [".", ".", ".", ".", ".", ".", "."]]
 
 
-def quadsErmitteln():
-    quads = {}
-    j = 0
-    bekannte_positionen = set()
-    for i in range(ZELLEN):
-        for richtung in RICHTUNGEN:
+
+    # Diese Funktion findet die Zeile, in die der Spielstein fällt.
+    def findeTiefsteZeile(spalte):
+        for zeile in reversed(range(ZEILEN)):
+            if (spalte, zeile) not in spielfeld:
+                return zeile
+
+
+    # Diese Funktion überprüft, ob die eingegebene Spalte gültig ist, der Wert für die Spalte darf 0-6 sein.
+    def spalteGueltig(spalte):
+        if (spalte, 0) in spielfeld:
+            return False
+        if 0 <= spalte < 7:
+            return True
+
+
+    # Diese Funktion gibt das aktuelle Spielfeld aus. Ein '.' steht für ein leeres Feld, in 'O' für einen Stein von
+    # Spieler 1 und ein 'X' für einen Stein von Spieler2.
+    def printSpielfeld():
+        for i in range(ZELLEN):
+            if i % SPALTEN == 0:
+                print()
             position = (i % SPALTEN, i // SPALTEN)
-            positionen = quadPositionen(position, richtung)
-            if not positionen or positionen in bekannte_positionen:
-                continue
-            quads[j] = [0, 0] # Anzahl der gelben und roten Steine im Quad
-            bekannte_positionen.add(frozenset(positionen))
-            j += 1
-    return quads
-'''
-# Diese Funktion findet die Zeile, in die der Spielstein fällt.
-def findeTiefsteZeile(spalte):
-    for zeile in reversed(range(ZEILEN)):
-        if (spalte, zeile) not in spielfeld:
-            return zeile
-
-
-# Diese Funktion überprüft, ob die eingegebene Spalte gültig ist, der Wert für die Spalte darf 0-6 sein.
-def spalteGueltig(spalte):
-    if (spalte, 0) in spielfeld:
-        return False
-    if 0 <= spalte < 7:
-        return True
-
-
-# Diese Funktion gibt das aktuelle Spielfeld aus. Ein '.' steht für ein leeres Feld, in 'O' für einen Stein von
-# Spieler 1 und ein 'X' für einen Stein von Spieler2.
-def printSpielfeld():
-    for i in range(ZELLEN):
-        if i % SPALTEN == 0:
-            print()
-        position = (i % SPALTEN, i // SPALTEN)
-        if position in spielfeld:
-            print(spielfeld[position], end=' ')
-        else:
-            print('.', end=' ')
-    print()
+            if position in spielfeld:
+                print(spielfeld[position], end=' ')
+            else:
+                print('.', end=' ')
+        print()
 
 
 # Diese Funktion überprüft, ob der jeweilige Spieler, der den letzten Stein geworfen hat, gewonnen hat.
@@ -83,21 +69,51 @@ def gewonnen(spieler):
                 print(f'Spieler ' + stein, 'hat gewonnen!')
                 return True
 
+class Spieler:
+    """
 
-# Diese Funktion überprüft, ob das Spielfeld voll ist und gibt dann aus: UNENTSCHIEDEN
-def voll():
-    i = 0
-    while i <= 6:
-        if spielfeld[i][5] != '.':
-            i += 1
+    """
+    def __init__(self, name: str, spielmodus: int):
+        self.__name = name
+        if spielmodus == 1 or spielmodus == 2:
+            self.__spielmodus = spielmodus
         else:
+            raise ValueError("Wähle 1 für Mensch vs. Mensch oder 2 für Mensch vs. Computer")
+
+    def __repr__(self):
+        return f"Name: {self.__name}, Spielmodus: {self.__spielmodus}"
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value: str):
+
+
+
+
+class Spiel:
+    """
+
+
+    """
+    def __init__(self):
+        self.__spieler1
+
+    # Diese Funktion überprüft, ob das Spielfeld voll ist und gibt dann aus: UNENTSCHIEDEN
+    def voll():
+        i = 0
+        spielfeld = field.getFields()
+        for list in spielfeld:
+            for item in list:
+                if item != ".":
+                    i += 1
+        if i < 42:
+            return False
+        else:
+            print("Spielfeld ist voll. UNENTSCHIEDEN!!!")
             return True
-        print("Spielfeld ist voll. UNENTSCHIEDEN!!!")
-        return False
-
-
-spieler = True
-
 
 if __name__ == '__main__':
     '''
@@ -122,6 +138,6 @@ if __name__ == '__main__':
         spielfeld[(spalte, zeile)] = 'O' if spieler else 'X'
         if gewonnen(spieler):
             break
-        if voll():
+        if not voll():
             break
         spieler = not spieler
